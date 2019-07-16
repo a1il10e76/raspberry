783282436bff6d0382c61ffd69094bdf93cb24d0
a1il10e76 <30648968+a1il10e76@users.noreply.github.com>
2019-07-16 23:39:27 +0900

    Add files via upload

--- list.py
+++ list.py
@@ -1,13 +1,15 @@
 #coding:utf-8
-import Tkinter
+import tkinter
+from tkinter import *
+from tkinter import ttk
 def btn_click(event):
-	if event.widget['bg'] == '#FF0000':
-		event.widget['bg'] = '#FFFFFF'
-	else:
-		event.widget['bg'] = '#FF0000'
-		#number.delete(2,Tkinter.END)
+    if event.widget['bg'] == '#FF0000':
+        event.widget['bg'] = '#FFFFFF'
+    else:
+        event.widget['bg'] = '#FF0000'
+        hancho_box[0].current(0)
 
-root = Tkinter.Tk()
+root = Tk()
 root.geometry('500x500')
 root.title('出勤表')
 
@@ -15,7 +17,14 @@ root.title('出勤表')
 kacho = []
 kakaricho = []
 hancho = []
+hancho_box = []
 member = []
+member_box = []
+kacho_number = [2505]
+kakaricho_number = [2501,2502]
+number = ['----','昼勤',2508,2509,2522,'遅番',2524,2523,2524,'夜勤',2507,2503,2525]
+late_number = []
+night_number = []
 #名前入力
 kacho_list = ['佐藤']
 kakaricho_list = ['木原','樫本']
@@ -27,18 +36,20 @@ hancho_num = len(hancho_list)
 member_num = len(member_list)
 #ボタン定義
 for i in range(kacho_num):
-	kacho.append(Tkinter.Button(root,text = kacho_list[i] + '課長',bg = '#FF0000',width = 4))
+    kacho.append(tkinter.Button(root,text = kacho_list[i] + '課長',bg = '#FF0000',width = 4))
 for i in range(kakaricho_num):
-    kakaricho.append(Tkinter.Button(root,text = kakaricho_list[i] + '係長',bg = '#FF0000',width = 4))
+    kakaricho.append(tkinter.Button(root,text = kakaricho_list[i] + '係長',bg = '#FF0000',width = 4))
 for i in range(hancho_num):
-    hancho.append(Tkinter.Button(root,text = hancho_list[i] + '班長',bg = '#FF0000',width = 4))
+    hancho.append(tkinter.Button(root,text = hancho_list[i] + '班長',bg = '#FF0000',width = 4))
+    hancho_box.append(ttk.Combobox(root,state='readonly',width = 5,values=(number)))
 for i in range(member_num):
-    member.append(Tkinter.Button(root,text = member_list[i],bg = '#FF0000',width = 4))
+    member.append(tkinter.Button(root,text = member_list[i],bg = '#FF0000',width = 4))
+    member_box.append(ttk.Combobox(root,state='readonly',width = 5,values=(number)))
 #ボタン配置
 kacho[0].grid(row = 8,column = 0)
 kakaricho[0].grid(row = 4,column = 1)
 kakaricho[1].grid(row = 12,column = 1)
-hancho[0].grid(row = 1,column = 3)
+hancho[0].grid(row = 1,column = 3); hancho_box[0].grid(row = 1,column = 4)
 hancho[1].grid(row = 3,column = 3)
 hancho[2].grid(row = 6,column = 3)
 hancho[3].grid(row = 14,column = 3)
@@ -54,14 +65,14 @@ member[8].grid(row = 12,column = 4)
 member[9].grid(row = 13,column = 4)
 member[10].grid(row = 15,column = 4)
 member[11].grid(row = 16,column = 4)
-member[12].grid(row = 1,column = 4)
+member[12].grid(row = 1,column = 5)
 #ボタンクリック処理
 for i in range(kacho_num):
-	kacho[i].bind('<1>',btn_click)
+    kacho[i].bind('<1>',btn_click)
 for i in range(kakaricho_num):
-	kakaricho[i].bind('<1>',btn_click)
+    kakaricho[i].bind('<1>',btn_click)
 for i in range(hancho_num):
-	hancho[i].bind('<1>',btn_click)
+    hancho[i].bind('<1>',btn_click)
 for i in range(member_num):
-	member[i].bind('<1>',btn_click)
+    member[i].bind('<1>',btn_click)
 root.mainloop()
